e859116a975ecb5946c5da88ef167318af771247
nicolas-moguilner <nicolas.moguilner@proquest.com>
2019-06-05 19:23:27 -0300

    nested categories dict to nested arrays for ngFor

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProductService} from "../services/product.service";
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  categories = [];
 
-  ngOnInit() {
+  constructor(private productService: ProductService) {
   }
 
+  ngOnInit() {
+    this.getCategories();
+  }
+
+  getCategories() {
+    this.productService.getCategories().subscribe(categories => {
+      this.dicToList(categories);
+    });
+  }
+
+  private dicToList(dictionary) {
+
+    let recursive = (dict, categoryItem: CategoryItem) => {
+
+      const keys = Object.keys(dict);
+      categoryItem.depth++;
+      categoryItem.children.push(...keys.map(k => new CategoryItem(k, [], categoryItem.depth)));
+
+      for (let childNode of categoryItem.children) {
+
+        const key = childNode.name;
+        const children = Object.keys(dict[key]);
+
+        if (!!children.length) {
+          recursive(dict[key], childNode);
+        }
+      }
+    }
+
+    const rootNode = new CategoryItem('root');
+    recursive(dictionary, rootNode);
+
+    console.log(rootNode);
+
+  }
+
+}
+
+export class CategoryItem {
+  public name: string;
+  public depth: number;
+  public children: CategoryItem[];
+
+  constructor(name: string, children: CategoryItem[] = [], depth = -1) {
+    this.name = name;
+    this.depth = depth;
+    this.children = children;
+  }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,27 +1,31 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { Injectable } from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Injectable} from '@angular/core';
 
 @Injectable()
 export class ProductService {
 
-   private baseProductUrl = "http://127.0.0.1:5000/products";
-    
-   constructor(private http: HttpClient) {
-    }
+  private baseProductUrl = "http://127.0.0.1:5000/products";
 
-    public getProducts(): Observable<any> {
-        return this.http.get(this.baseProductUrl);
-    }
+  constructor(private http: HttpClient) {
+  }
 
-    public getProductByAsin(asin: string) {
-        return this.http.get(`${this.baseProductUrl}/${asin}`);
-    }
+  public getProducts(): Observable<any> {
+    return this.http.get(this.baseProductUrl);
+  }
 
-    public getRecommendations(asin, topN): Observable<any> {
-        let params = new HttpParams().set('topN', topN);
-        return this.http.get(`${this.baseProductUrl}/${asin}/best`, {params});
-    }
+  public getProductByAsin(asin: string) {
+    return this.http.get(`${this.baseProductUrl}/${asin}`);
+  }
+
+  public getRecommendations(asin, topN): Observable<any> {
+    let params = new HttpParams().set('topN', topN);
+    return this.http.get(`${this.baseProductUrl}/${asin}/best`, {params});
+  }
+
+  public getCategories(): Observable<any> {
+    return this.http.get(`${this.baseProductUrl}/categories`);
+  }
 
 
 }
